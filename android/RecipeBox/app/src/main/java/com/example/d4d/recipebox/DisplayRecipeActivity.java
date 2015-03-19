c02d4c880819972ef2659454f5603a7b616951b1
mat3049 (Aeolus) <mat3049@gmx.com>
2015-03-18 21:38:59 -0400

    Added basic View framework
    DisplayRecipeActivity framework with default values that repopulate with
    provided Recipe.
    
    Couple of TODOs:
    - Implement the ingredient list display (single column bulleted
    TextView)
    - Need int to String conversion for cuisine, meal type, etc.

--- android/RecipeBox/app/src/main/java/com/example/d4d/recipebox/DisplayRecipeActivity.java
+++ android/RecipeBox/app/src/main/java/com/example/d4d/recipebox/DisplayRecipeActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 
 public class DisplayRecipeActivity extends ActionBarActivity {
@@ -26,6 +28,36 @@ public class DisplayRecipeActivity extends ActionBarActivity {
             Log.e(DEBUG_TAG, "Not passed a Recipe, aborting");
             return;
         }
+
+        // enumerates all modifiable views
+        ImageView recipepicture = (ImageView) findViewById(R.id.recipePicture);
+        TextView recipename = (TextView) findViewById(R.id.recipeName);
+        TextView recipeinstructions = (TextView) findViewById(R.id.recipeInstructions);
+        TextView recipedescription = (TextView) findViewById(R.id.recipeDescription);
+
+        // TODO: check if this needs to a TableView with TextView inside -- how many columns/entries?
+        TextView recipeingredientlist = (TextView) findViewById(R.id.recipeIngredientList);
+
+        TextView recipecuisine = (TextView) findViewById(R.id.recipeCuisine);
+        TextView recipemealtype = (TextView) findViewById(R.id.recipeMealType);
+        TextView recipeseason = (TextView) findViewById(R.id.recipeSeason);
+
+        // sets the values of modifiable views to reflect those of the provided recipe
+        recipepicture.setImageBitmap(recipe.getPicture());
+        recipename.setText(recipe.getCuisine());
+        recipeinstructions.setText(recipe.getInstructions());
+        recipedescription.setText(recipe.getDescription());
+
+        // TODO: fix the List<Strings> to bulleted list ja.
+        recipeingredientlist.setText("INGREDIENT LIST GOES HERE");
+
+        // TODO: convert these int values to String values!
+        // TODO: using String.valueOf(int values) ATM
+        recipecuisine.setText(String.valueOf(recipe.getCuisine()));
+        recipemealtype.setText(String.valueOf(recipe.getMealType()));
+        recipeseason.setText(String.valueOf(recipe.getSeason()));
+
+
 
     }
 
